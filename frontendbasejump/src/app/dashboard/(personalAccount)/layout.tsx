3ca8d1d9e988e0240412450658983afdfb46d395
Adam Cohen Hillel <adamcohenhillel@gmail.com>
2025-04-11 13:51:29 +0100

    basejumo frontend

---
+++ frontendbasejump/src/app/dashboard/(personalAccount)/layout.tsx
@@ -0,0 +1,36 @@
+import {createClient} from "@/lib/supabase/server";
+import DashboardHeader from "@/components/dashboard/dashboard-header";
+
+export default async function PersonalAccountDashboard({children}: {children: React.ReactNode}) {
+
+    const supabaseClient = createClient();
+
+    const {data: personalAccount, error} = await supabaseClient.rpc('get_personal_account');
+
+    const navigation = [
+        {
+            name: 'Overview',
+            href: '/dashboard',
+        },
+        {
+            name: 'Devices',
+            href: '/dashboard/devices',
+        },
+        {
+            name: 'Recordings',
+            href: '/dashboard/recordings',
+        },
+        {
+            name: 'Settings',
+            href: '/dashboard/settings'
+        }
+    ]
+
+    return (
+        <>
+            <DashboardHeader accountId={personalAccount.account_id} navigation={navigation} />
+            <div className="w-full p-8">{children}</div>
+        </>
+    )
+
+}
---
+++ frontendbasejump/src/app/dashboard/[accountSlug]/recordings/layout.tsx
@@ -0,0 +1,7 @@
+export default function RecordingsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className="space-y-6">{children}</div>;
+} 
