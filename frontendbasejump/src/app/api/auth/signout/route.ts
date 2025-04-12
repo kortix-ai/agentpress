f6c4da9f96ac060eef3478d1efece8c8723f6d3c
Adam Cohen Hillel <adamcohenhillel@gmail.com>
2025-04-12 01:04:40 +0100

    clean

--- frontendbasejump/src/app/api/auth/signout/route.ts
+++ frontendbasejump/src/app/api/auth/signout/route.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@/lib/supabase/server";
-import { redirect } from "next/navigation";
+
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
